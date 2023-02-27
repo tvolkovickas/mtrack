1cc86b914bf0c25e027283e525c365726f77e095
tvolkovickas <tvolkovickas@gmail.com>
2023-02-27 11:03:35 +0000

    New content and minor updates

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,38 +7,51 @@ import {
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const [counter, setCounter] = useState(null);
   const row1 = [
     "3 mm",
-    "Fine Dust",
+    "FINE DUST",
+    "SLURRY DUST",
     "6 mm",
     "10 mm",
     "14 mm",
     "20 mm",
-    "28 mm",
   ];
   const row2 = [
-    "",
-    "Slurry Dust",
+    "28 mm",
+    "40 mm",
+    "RAIL BALLAST",
     "6 mm BEST",
     "10 mm BEST",
     "14 mm BEST",
     "20 mm BEST",
-    "",
   ];
   const row3 = [
-    "40 mm",
-    "Rail + Ballas",
-    "Formpave",
-    "Type 3",
-    "Blend 66",
-    "75 / 40",
-    "20 / 5",
+    "TYPE 1",
+    "TYPE 3",
+    "BLEND 66",
+    "BLEND 20/5",
+    "FORMPAVE",
+    "75/40",
+    "OVERSIZED",
   ];
-  const row4 = ["ROAD BLEND", "WGS", "Oversized", "6F5", "10S/D", "6S/D", ""];
+  const row4 = [
+    "ROAD BLEND",
+    "WASHED GRANIT SAND",
+    "GF5",
+    "6 mm SURFACE DRESSING",
+    "10 mm SURFACE DRESSING",
+    "",
+    "",
+  ];
 
   const stopCounter = () => {
     setCounter(null);
@@ -63,8 +76,32 @@ export default function App() {
       clearTimeout(timer1);
     };
   }, [counter]);
+
+  useEffect(() => {
+    async function prepare() {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setAppIsReady(true);
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <SafeAreaView style={{ height: "100%" }}>
+    <SafeAreaView style={{ height: "100%" }} onLayout={onLayoutRootView}>
       <StatusBar style="auto" />
       {selectedItem && (
         <TouchableOpacity style={styles.on} onPress={stopCounter}>
@@ -162,14 +199,14 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     height: "25%",
-    margin: 10,
+    marginTop: 10,
   },
   gridItemWrapper: {
     width: "14.2%",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "grey",
+    backgroundColor: "#595959",
     margin: 5,
   },
   gridItem: {
@@ -177,9 +214,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     padding: 5,
+    fontWeight: "bold",
   },
   on: {
-    backgroundColor: "green",
+    backgroundColor: "#06601D",
     margin: 30,
   },
   selectedItem: {
@@ -187,6 +225,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 32,
     padding: 20,
+    fontWeight: "bold",
   },
   counter: {
     flexDirection: "row",
@@ -195,9 +234,11 @@ const styles = StyleSheet.create({
   counterDigit: {
     color: "white",
     fontSize: 28,
+    fontWeight: "bold",
   },
   blinker: {
     color: "white",
     fontSize: 28,
+    fontWeight: "bold",
   },
 });
